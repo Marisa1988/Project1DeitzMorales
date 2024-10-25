d9cd6adc13dfe36b99229ae5788c152046f053fa
Marisabel Morales <moralesmurallesm@ufl.edu>
2024-10-25 11:02:15 -0400

    Update FoodData.py

--- FoodData.py
+++ FoodData.py
@@ -146,9 +146,10 @@ plt.xlabel('Number of Products')
 plt.ylabel('Category')
 plt.title('Top 25 Major Categories in Open Food Facts')
 plt.gca().invert_yaxis()  # Invert y-axis to have the category with the most products on top
-plt.show()
+# plt.show()
 plt.savefig('plot1.png', dpi=300) 
 plt.show()
+plt.close()
 
 # In[11]:
 
